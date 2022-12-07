3ebce9fd4effcba1af10c191fba30f5fa8478639
Yendak <yan.maia93@gmail.com>
2022-12-07 11:28:44 -0400

    atualizando

--- scripts/index.js
+++ scripts/index.js
@@ -1,147 +1,60 @@
-///Desativando botão de acessar
+//------------------------------------------- IMPORTAÇÕES
+import loginUser from './requisicoes/loginUser.js'
+import { loading } from '../constants/loading.js'
+import { limparValorDeObjetos } from '../constants/limparCampos.js'
+import verSenha from '../constants/mostrarSenha.js'
 
-function checkInputs(inputs) {
+//------------------------------------------- VARIÁVEIS LOCAIS
+// Selecionando o botão para fazer login
+const loginUserButtonElement = document.querySelector('#loginUserButton')
+// Selecionando todos as tags com a classe form-control
+const inputElement = document.querySelectorAll('.form-control')
 
-  var filled = true;
-
-inputs.forEach(function(input) {
-  
-  if(input.value === "") {
-      filled = false;
-  }
-
-  });
-
-return filled;
-
+// Para armazenar os dados que são digitados no formulário de login
+let formValidationLogin = {
+  email: false,
+  password: false
 }
 
-var inputs = document.querySelectorAll("input");
-var button = document.querySelector("button");
+//------------------------------------------- INÍCIO FUNÇÃO PARA CONTROLAR OS DADOS
+const index = () => {
+  for (let control of inputElement) {
+    const controlInputElement = control.children[1]
 
-inputs.forEach(function(input) {
-  
-input.addEventListener("keyup", function() {
+    controlInputElement.addEventListener('keyup', event => {
+      let inputValid = event.target.checkValidity()
+      let inputValue = event.target.value.trim()
 
-  if(checkInputs(inputs)) {
-    button.disabled = false;
-    button.style.opacity = ""
-  } else {
-    button.disabled = true;
-    button.style.opacity = "35%"
-  }
+      if (inputValid) {
+        formValidationLogin[event.target.id] = inputValue
+        formValidationLogin.email = formValidationLogin.email.toLowerCase()
 
-});
-
-});
-
-
-////////Validando O E-MAIL
-let btn = document.getElementById("botao");
-btn.setAttribute("disabled", true);
-let email = document.getElementById("inputEmail")
-let validaEmail = false
-
-email.addEventListener("focus", function () {
-  email.style.backgroundColor = "#E2DCDC"
-});
-
-//Blur ou KeyUp
-email.addEventListener("keyup", function () {
-  console.log("Saiu do campo do email");
-
-  //Pega o elemento Small
-  let emailValidation = document.getElementById('emailValidation');
-
-
-  //Altera o fundo do campo ao sair do click 
-  email.style.backgroundColor = "#FFFFFF"
-
-  //Valida se o campo É VAZIO
-  if (!email.value) { //Se não foi preenchido
-    // troca o fundo
-    //   email.style.border = "solid 1.5px #13A02D";
-
-    //Seta a mensage no small (e configura)
-    emailValidation.innerText = "Campo obrigatório"
-    emailValidation.style.color = "#D53A3A"
-    emailValidation.style.fontWeight = "bold"
-
-    //Bloqueado o botão de salvar
-    //   botaoCapturado.setAttribute("disabled", true);
-    //Trocando a cor do botão
-    //   botaoCapturado.style.backgroundColor = "#908E8E"
-
-    //Seta a variável para falso
-    //   emailFormularioOk = false;
-
-    //Valida de o email está em um formato correto
-  } else if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) { //Testa o erro
-
-    // troca o fundo
-    email.style.border = "solid 1.5px #D8341B";
-
-    //Seta a mensage no small (e configura)
-    emailValidation.innerText = "E-mail inválido"
-    emailValidation.style.color = "#D53A3A"
-    emailValidation.style.fontWeight = "bold"
-
-    //Bloqueado o botão de salvar
-    //   botaoCapturado.setAttribute("disabled", true);
-    //Trocando a cor do botão
-    //   botaoCapturado.style.backgroundColor = "#908E8E"
-
-    //Seta a variável para falso
-    //   emailFormularioOk = false;
-
-  } else {
-    validaEmail = true
-    //Se o campo estiver ok
-    //Retira a mensage do small
-    emailValidation.innerText = ""
-
-    //Seta o BG do input como Sucesso
-    email.style.border = "solid 1.5px #13A02D";
-
-    //Seta a variável de controlle do formulário
-    //   emailFormularioOk = true;
-
-    //   if (nomeFormularioOk && emailFormularioOk) {
-    //     //Retorna o botão para "habilitado"
-    //     botaoCapturado.removeAttribute("disabled");
-    //     botaoCapturado.style.backgroundColor = "#0b5ed7"
-    //   }
-  }
-
-});
-
-// validando senha:
-
-let validaSenha = document.getElementById("senhaValidation")
-let senha = document.getElementById("inputPassword");
-let validacaoSenha = false;
-
-senha.addEventListener("focus", function () {
-  senha.style.backgroundColor = "#E2DCDC"
-});
-senha.addEventListener("keyup", function () {
-  senha.value.toString()
-  if ((senha.value)) {
-
-    validaSenha.innerText = "Campo obrigatório"
-    validaSenha.style.color = "#D53A3A"
-    validaSenha.style.fontWeight = "bold"
-    console.log(senha.value.length > 8)
-  }
-
-  if (senha.value.length >= 8) {
-    validaSenha.innerText = ""
-    validacaoSenha = true
+        control.classList.remove('error')
+      } else {
+        control.classList.add('error')
+      }
+      //Habilitando o botão após os campos estarem validados
+    if(formValidationLogin.email && formValidationLogin.password){
+      loginUserButtonElement.disabled = false
+    }
+    })
     
   }
 
+  //função para mostrar a senha
+verSenha()
 
-})
-if ((validaEmail=true) && (validacaoSenha=true)){
-  btn.removeAttribute("disabled")
+//------------------------------------------- FUNÇÃO DE CLIQUE NO BOTÃO
+
+  loginUserButtonElement.addEventListener('click', event => {
+    event.preventDefault()
+    //Iniciando o loading
+    loading()
+    //Enviando os dados obtidos para realizar a requisição de login
+    loginUser(formValidationLogin)
+    //Função para deixar os campos limpos para se caso retorne a página, os campos estejam limpos e sem cache
+    limparValorDeObjetos(inputElement)
+  })
 }
+
+export default index
