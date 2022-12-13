f00b0dd1b11ee393162f040f4ac4f6eba4d51555
CaioNunesS <caiogrb@gmail.com>
2022-12-13 00:49:44 -0300

    trim no signup

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,13 +2,21 @@ function normalizaEmail(entrada){
     entrada.toLowerCase()
     return entrada.trim();
     
-}
+};
 
 function normalizaSenha(senha){
     return senha.trim()
     
-}
+};
 
 function baseUrl(){
     return "http://todo-api.ctd.academy:3000/v1"
-}
+};
+
+function normalizeLastName(sobrenome){
+    return sobrenome.trim() 
+};
+
+function normalizeInputName(nome){
+    return nome.trim()  
+};
